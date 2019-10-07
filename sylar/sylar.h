c67218ace32ecfe4bfb2fd2809240381e171dd09
root <root@DESKTOP-69UV2B0.localdomain>
2019-10-07 14:16:01 +0800

    实现线程模块

---
+++ sylar/sylar.h
@@ -0,0 +1,10 @@
+#ifndef MYSYLAR_SYLAR_H
+#define MYSYLAR_SYLAR_H
+
+#include "sylar/config.h"
+#include "sylar/log.h"
+#include "sylar/singleton.h"
+#include "sylar/thread.h"
+#include "sylar/util.h"
+
+#endif
---
+++ sylar/thread.h
@@ -0,0 +1,45 @@
+#ifndef MYSYLAR_THREAD_H
+#define MYSYLAR_THREAD_H
+
+#include <functional>
+#include <memory>
+#include <pthread.h>
+#include <string>
+#include <thread>
+
+namespace sylar {
+
+class Thread {
+public:
+    typedef std::shared_ptr<Thread> ptr;
+    Thread(std::function<void()> cb, const std::string &name);
+    ~Thread();
+
+    pid_t getId() const { return m_id; }
+    const std::string getName() const { return m_name; }
+
+    void join();
+
+    static Thread *GetThis();
+    static const std::string &GetName();
+    static void SetName(const std::string &name);
+
+private:
+    //禁止线程类对象拷贝
+    Thread(const Thread &)  = delete;
+    Thread(const Thread &&) = delete;
+    Thread operator=(const Thread &) = delete;
+
+    //线程入口
+    static void *run(void *arg);
+
+private:
+    pid_t m_id         = -1;
+    pthread_t m_thread = 0;
+    std::function<void()> m_cb; //线程回调函数
+    std::string m_name;
+};
+
+} // namespace sylar
+
+#endif
