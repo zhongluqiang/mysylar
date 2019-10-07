aacd7f63532377c2f4abd8f587fcd4afd2bc7086
root <root@DESKTOP-69UV2B0.localdomain>
2019-10-07 14:59:48 +0800

    信号量和读写锁实现

--- sylar/thread.cpp
+++ sylar/thread.cpp
@@ -10,6 +10,26 @@ static thread_local std::string t_thread_name = "UNKNOWN";
 
 static sylar::Logger::ptr g_logger = SYLAR_LOG_NAME("system");
 
+Semaphore::Semaphore(uint32_t count) {
+    if (sem_init(&m_semaphore, 0, count)) {
+        throw ::std::logic_error("sem_init error");
+    }
+}
+
+Semaphore::~Semaphore() { sem_destroy(&m_semaphore); }
+
+void Semaphore::wait() {
+    if (sem_wait(&m_semaphore)) {
+        throw std::logic_error("sem_wait error");
+    }
+}
+
+void Semaphore::notify() {
+    if (sem_post(&m_semaphore)) {
+        throw std::logic_error("sem_post error");
+    }
+}
+
 Thread *Thread::GetThis() { return t_thread; }
 
 const std::string &Thread::GetName() { return t_thread_name; }
--- tests/test_thread.cpp
+++ tests/test_thread.cpp
@@ -3,13 +3,20 @@
 
 sylar::Logger::ptr g_logger = SYLAR_LOG_ROOT();
 
+int count = 0;
+sylar::RWMutex s_mutex;
+
 void func1() {
     SYLAR_LOG_INFO(g_logger)
         << " name:" << sylar::Thread::GetName()
         << " this.name:" << sylar::Thread::GetThis()->getName()
         << " id: " << sylar::GetThreadId()
         << " this.id: " << sylar::Thread::GetThis()->getId();
-    sleep(20);
+    // sleep(20);
+    for (int i = 0; i < 10000000; ++i) {
+        sylar::RWMutex::WriteLock lock(s_mutex);
+        ++count;
+    }
 }
 
 int main() {
@@ -27,5 +34,6 @@ int main() {
     }
 
     SYLAR_LOG_INFO(g_logger) << "thread test end";
+    SYLAR_LOG_INFO(g_logger) << "count=" << count;
     return 0;
 }
